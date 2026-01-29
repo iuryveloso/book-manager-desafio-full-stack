c83b6d78f5662133068742db8ecbbc82cf198853
Iury Veloso <iurysveloso@gmail.com>
2026-01-29 14:30:33 -0300

    feat: enhance CORS configuration to support dynamic allowed origins

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,23 @@ import cookieParser from 'cookie-parser'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+
+  const allowedOrigins: string[] = process.env.ALLOWED_ORIGINS?.split(',') || []
+
   app.setGlobalPrefix('api')
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors({
-    origin: [process.env.FRONTEND_URL, process.env.FRONTEND_URL_DEVELOP],
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true)
+      } else {
+        callback(new Error('Not allowed by CORS'))
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   })
   app.use(cookieParser())
